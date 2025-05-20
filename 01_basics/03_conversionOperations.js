2d4b6b370b48ebadb7ab2afa5edd8ef43db961ec
yashbhardwaj01 <124570278+yashbhardwaj01@users.noreply.github.com>
2025-05-20 06:35:57 +0000

    Conversion of Datatypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,13 @@
+let score = "11abc"
+
+console.log(typeof score)
+console.log(typeof (score))
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+let isLoggedIn = 0
+let booleanIsloggedIn = Boolean(isLoggedIn)
+console.log(booleanIsloggedIn)
+
