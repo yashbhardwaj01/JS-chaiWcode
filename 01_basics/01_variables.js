31b344a0e00dd03582b6680c780e798c639b8f8d
yashbhardwaj01 <124570278+yashbhardwaj01@users.noreply.github.com>
2025-05-20 06:09:16 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 123456
+let accountName = "yash"
+var accountMail = "yb123@gmail.com"
+accountCity = "aligarh"
+accountName = "ram"
+// accountId = 4566
+
+// console.table([accountId , accountName , accountMail , accountCity]);
+
+let accounttype;
+console.table([accountId , accountName , accountMail , accountCity , accounttype]);
