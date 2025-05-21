62d5a55db86019e23991531708f9e7e3170c0039
yashbhardwaj01 <124570278+yashbhardwaj01@users.noreply.github.com>
2025-05-21 06:03:12 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,18 @@
+const name = "yash"
+const repoCount = 10
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+// string creation 
+
+const mystring = new String('yashb')
+// console.log(mystring);
+// console.log(mystring[2]);
+// console.log(mystring.__proto__);
+
+// console.log(mystring.substring(0,4));
+console.log(mystring.slice(0,-2));
+
+const url = "https://super-zebra-v4jjpxxjpgr2pgj7.github.dev/"
+console.log(url.replace('zebra' , 'yashu'));
+console.log(url.includes('zebra'));
